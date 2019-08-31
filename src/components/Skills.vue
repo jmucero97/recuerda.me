29d8ddbc8daa954f54d4c038c52333167c71981c
juanmaucero97 <jm120897@gmail.com>
2019-09-01 00:24:37 +0200

    v-for & v-if/else

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,10 +1,13 @@
 <template>
   <div class="hello">
-    {{ name }}
+    <div class="holder">
+      <ul>
+        <li v-for="(data,index) in skills" :key='index'>{{index}}. {{data.skill}}</li>
+      </ul>
 
-    {{ btnState? 'The button is disabled' : 'The button is enabled'}}
-
-    <button v-on:click="changeName" v-bind:disabled="btnState">Change Name</button>
+      <p v-if="skills.length >= 1">You have more than 1 skill</p>
+      <p v-else>You have less than or equal to 1 skill</p>
+    </div>
   </div>
 </template>
 
